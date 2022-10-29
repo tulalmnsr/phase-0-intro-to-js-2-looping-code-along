5be7bfc61d353a0c70899813ed31a0922c7f4b43
Batoul El Mansour <elmansourbatoul@gmail.com>
2022-10-29 23:52:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const message=[]
+function writeCards(names,event){
+    for (let i = 0; i < names.length; i++){
+      message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+      debugger;
+    }
+    return message
+}
+function countDown(num){
+    let countDown = num;
+while (countDown >=0) {
+  console.log(countDown--);
+}
+}
